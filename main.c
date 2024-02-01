76effc9b30142db10879e804a0c87bddce90f9d4
Jacky Li <drsl@drshapeless.com>
2024-02-01 22:58:07 +0800

    Finish create surface

--- main.c
+++ main.c
@@ -33,7 +33,31 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance,
 
 void populateDebugMessengerCreateInfo(
     VkDebugUtilsMessengerCreateInfoEXT *createInfo);
-bool isDeviceSuitable(VkPhysicalDevice device);
+bool isDeviceSuitable(VkPhysicalDevice device, VkSurfaceKHR surface);
+struct QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device,
+                                            VkSurfaceKHR surface);
+
+struct QueueFamilyIndices {
+    /* the vulkan tutorial uses a optional type in C++17 which we
+       don't have in C */
+
+    /* use a bool to indicate whether it has value */
+    uint32_t graphicsFamily;
+    bool graphicsFamilyHasValue;
+
+    uint32_t presentFamily;
+    bool presentFamilyHasValue;
+
+    /* there is a isComplete member function here, we don't have it in
+       C */
+};
+
+/* ugly workaround of the OO features in the original tutorial */
+bool QueueFamilyIndicesIsComplete(
+    struct QueueFamilyIndices *queueFamiliyIndices) {
+    return queueFamiliyIndices->graphicsFamilyHasValue &&
+           queueFamiliyIndices->presentFamilyHasValue;
+}
 
 int main(int argc, char *argv[]) {
     int rc = 0;
@@ -42,6 +66,9 @@ int main(int argc, char *argv[]) {
     VkInstance instance = NULL;
     VkDebugUtilsMessengerEXT debugMessenger = NULL;
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkDevice device = NULL;
+    VkQueue graphicsQueue = NULL;
+    VkSurfaceKHR surface = NULL;
 
     /* init sdl */
     rc = SDL_Init(SDL_INIT_VIDEO);
@@ -62,80 +89,81 @@ int main(int argc, char *argv[]) {
 
     /* init vulkan */
     /* create instance */
-    if (enableValidationLayers && !checkValidationLayerSupport()) {
-        error_log("validation layers requested, but not available");
-        return 1;
-    }
-    VkApplicationInfo appInfo = { 0 };
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Hello Triangle";
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "No Engine";
-    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+    {
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            error_log("validation layers requested, but not available");
+            return 1;
+        }
+        VkApplicationInfo appInfo = { 0 };
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    VkInstanceCreateInfo createInfo = { 0 };
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
+        VkInstanceCreateInfo createInfo = { 0 };
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
 
-    uint32_t sdlExtensionCount = 0;
-    const char **sdlExtensions = NULL;
+        uint32_t sdlExtensionCount = 0;
+        const char **sdlExtensions = NULL;
 
-    if (SDL_Vulkan_GetInstanceExtensions(window, &sdlExtensionCount,
-                                         sdlExtensions) == SDL_FALSE) {
-        error_log("cannot get instance extensions");
-        error_log(SDL_GetError());
-        return 1;
-    }
+        if (SDL_Vulkan_GetInstanceExtensions(window, &sdlExtensionCount,
+                                             sdlExtensions) == SDL_FALSE) {
+            error_log("cannot get instance extensions");
+            error_log(SDL_GetError());
+            return 1;
+        }
 
-    uint32_t extensionCount = 0;
-    /* This should be big enough to hold all the extensions. */
-    vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
-    const char **extensions = malloc(sizeof(char **) * extensionCount);
-    int extensionIndex = 0;
+        uint32_t extensionCount = 0;
+        /* This should be big enough to hold all the extensions. */
+        vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
+        const char **extensions = malloc(sizeof(char **) * extensionCount);
+        int extensionIndex = 0;
 
 #ifdef __APPLE__
-    extensions[0] = VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME;
-    extensions[1] = "VK_KHR_get_physical_device_properties2";
-    createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
-    extensionIndex += 2;
+        extensions[0] = VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME;
+        extensions[1] = "VK_KHR_get_physical_device_properties2";
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+        extensionIndex += 2;
 #endif
-    for (int i = 0; i < sdlExtensionCount; i++) {
-        extensions[extensionIndex + i] = sdlExtensions[i];
-    }
+        for (int i = 0; i < sdlExtensionCount; i++) {
+            extensions[extensionIndex + i] = sdlExtensions[i];
+        }
 
-    extensionIndex += sdlExtensionCount;
-    if (enableValidationLayers) {
-        /* This is from the original getRequiredExtensions() function,
+        extensionIndex += sdlExtensionCount;
+        if (enableValidationLayers) {
+            /* This is from the original getRequiredExtensions() function,
            c++ hide a lot of stuffs by extracting one-use function. */
-        extensions[extensionIndex] = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
-        extensionIndex += 1;
+            extensions[extensionIndex] = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+            extensionIndex += 1;
+        }
+
+        createInfo.enabledExtensionCount = extensionIndex;
+        createInfo.ppEnabledExtensionNames = extensions;
+
+        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo = { 0 };
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount =
+                sizeof(validationLayers) / sizeof(char *);
+            createInfo.ppEnabledLayerNames = validationLayers;
+
+            populateDebugMessengerCreateInfo(&debugCreateInfo);
+            createInfo.pNext =
+                (VkDebugUtilsMessengerCreateInfoEXT *)&debugCreateInfo;
+        } else {
+            createInfo.enabledLayerCount = 0;
+            createInfo.pNext = NULL;
+        }
+
+        if (vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS) {
+            error_log("failed to create instance");
+            return 1;
+        }
+
+        free(extensions);
     }
-
-    createInfo.enabledExtensionCount = extensionIndex;
-    createInfo.ppEnabledExtensionNames = extensions;
-
-    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo = { 0 };
-    if (enableValidationLayers) {
-        createInfo.enabledLayerCount =
-            sizeof(validationLayers) / sizeof(char *);
-        createInfo.ppEnabledLayerNames = validationLayers;
-
-        populateDebugMessengerCreateInfo(&debugCreateInfo);
-        createInfo.pNext =
-            (VkDebugUtilsMessengerCreateInfoEXT *)&debugCreateInfo;
-    } else {
-        createInfo.enabledLayerCount = 0;
-        createInfo.pNext = NULL;
-    }
-
-    if (vkCreateInstance(&createInfo, NULL, &instance) != VK_SUCCESS) {
-        error_log("failed to create instance");
-        return 1;
-    }
-
-    free(extensions);
-
     /* setup debug messenger */
     if (enableValidationLayers) {
         VkDebugUtilsMessengerCreateInfoEXT createInfo = { 0 };
@@ -161,32 +189,75 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    /* pick physical device */
-    uint32_t deviceCount = 0;
-    vkEnumeratePhysicalDevices(instance, &deviceCount, NULL);
-    if (deviceCount == 0) {
-        error_log("failed to find GPUs with Vulkan support!");
+    /* create surface */
+    if (SDL_Vulkan_CreateSurface(window, instance, &surface) != SDL_TRUE) {
+        error_log("failed to create window surface!");
         return 1;
     }
 
-    VkPhysicalDevice *devices =
-        malloc(sizeof(VkPhysicalDevice *) * deviceCount);
-    vkEnumeratePhysicalDevices(instance, &deviceCount, devices);
+    /* pick physical device */
+    {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, NULL);
+        if (deviceCount == 0) {
+            error_log("failed to find GPUs with Vulkan support!");
+            return 1;
+        }
 
-    for (int i = 0; i < deviceCount; i++) {
-        if (isDeviceSuitable(devices[i])) {
-            physicalDevice = devices[i];
-            break;
+        VkPhysicalDevice *devices =
+            malloc(sizeof(VkPhysicalDevice *) * deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices);
+
+        for (int i = 0; i < deviceCount; i++) {
+            if (isDeviceSuitable(devices[i], surface)) {
+                physicalDevice = devices[i];
+                break;
+            }
+        }
+
+        if (physicalDevice == VK_NULL_HANDLE) {
+            error_log("failed to find a suitable GPU!");
+            return 1;
+        }
+
+        free(devices);
+    }
+
+    /* create logical device */
+    {
+        struct QueueFamilyIndices indices =
+            findQueueFamilies(physicalDevice, surface);
+        VkDeviceQueueCreateInfo queueCreateInfo = { 0 };
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily;
+        queueCreateInfo.queueCount = 1;
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+
+        VkPhysicalDeviceFeatures deviceFeatures = { 0 };
+
+        VkDeviceCreateInfo createInfo = { 0 };
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = 1;
+
+        createInfo.pEnabledFeatures = &deviceFeatures;
+        createInfo.enabledExtensionCount = 0;
+
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount =
+                (uint32_t)(sizeof(validationLayers) / sizeof(char *));
+            createInfo.ppEnabledLayerNames = validationLayers;
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        if (vkCreateDevice(physicalDevice, &createInfo, NULL, &device) !=
+            VK_SUCCESS) {
+            error_log("failed to create logical device!");
+            return 1;
         }
     }
-
-    if (physicalDevice == VK_NULL_HANDLE) {
-        error_log("failed to find a suitable GPU!");
-        return 1;
-    }
-
-    free(devices);
-
     /* main loop */
     while (running) {
         /* process event */
@@ -201,10 +272,13 @@ int main(int argc, char *argv[]) {
     }
 
     /* clean up */
+    vkDestroyDevice(device, NULL);
+
     if (enableValidationLayers) {
         DestroyDebugUtilsMessengerEXT(instance, debugMessenger, NULL);
     }
 
+    vkDestroySurfaceKHR(instance, surface, NULL);
     vkDestroyInstance(instance, NULL);
 
     SDL_DestroyWindow(window);
@@ -265,7 +339,6 @@ VkResult CreateDebugUtilsMessengerEXT(
     PFN_vkCreateDebugUtilsMessengerEXT func =
         (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
             instance, "vkCreateDebugUtilsMessengerEXT");
-    printf("%p\n", func);
     if (func != NULL) {
         return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
     } else {
@@ -298,6 +371,40 @@ void populateDebugMessengerCreateInfo(
     createInfo->pfnUserCallback = debugCallback;
 }
 
-bool isDeviceSuitable(VkPhysicalDevice device) {
-    return true;
+bool isDeviceSuitable(VkPhysicalDevice device, VkSurfaceKHR surface) {
+    struct QueueFamilyIndices indices = findQueueFamilies(device, surface);
+
+    return QueueFamilyIndicesIsComplete(&indices);
+}
+struct QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device,
+                                            VkSurfaceKHR surface) {
+    struct QueueFamilyIndices indices = { 0 };
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, NULL);
+
+    VkQueueFamilyProperties *queueFamilies =
+        malloc(sizeof(VkQueueFamilyProperties) * queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount,
+                                             queueFamilies);
+    for (int i = 0; i < queueFamilyCount; i++) {
+        if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            indices.graphicsFamily = i;
+            /* work around of optional */
+            indices.graphicsFamilyHasValue = true;
+        }
+
+        VkBool32 presentSupport = false;
+        vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface,
+                                             &presentSupport);
+        if (presentSupport) {
+            indices.presentFamily = i;
+        }
+
+        if (QueueFamilyIndicesIsComplete(&indices)) {
+            break;
+        }
+    }
+
+    free(queueFamilies);
+    return indices;
 }
